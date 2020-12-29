a4ae972363fd3832b952660d2f69e8f719d8997e
Ondřej Marek <ondrej.marek10@gmail.com>
2020-12-29 13:05:48 +0100

    přidány třídy Question a QSet (sada otázek)

---
+++ Models/QSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Millionaire.Models
+{
+    class QSet
+    {
+        public string Name { get; set; }
+
+        List<Question> easyQuestions = new List<Question>();
+        List<Question> mediumQuestions = new List<Question>();
+        List<Question> hardQuestions = new List<Question>();
+
+
+        public List<Question> EasyQuestions { get { return easyQuestions; } }
+        public List<Question> MediumQuestions { get { return mediumQuestions; } }
+        public List<Question> HardQuestions { get { return hardQuestions; } }
+
+        //public QSet(string name)
+        //{
+        //    Name = name;
+        //}
+
+        public void AddEasyQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        {
+            easyQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+        }
+
+        public void AddMediumQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        {
+            mediumQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+        }
+
+        public void AddHardQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        {
+            hardQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+        }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Millionaire.Models
+{
+    class Question
+    {
+        public string question { get; set; }
+        public string rightAnswer { get; set; }
+        public string wrongAnswer1 { get; set; }
+        public string wrongAnswer2 { get; set; }
+        public string wrongAnswer3 { get; set; }
+
+        public Question(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        {
+            this.question = question;
+            this.rightAnswer = rightAnswer;
+            this.wrongAnswer1 = wrongAnswer1;
+            this.wrongAnswer2 = wrongAnswer2;
+            this.wrongAnswer3 = wrongAnswer3;
+        }
+
+
+    }
+}
