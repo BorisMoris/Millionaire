5b029878d7d21320650e8d64d69fe8f9c476d899
Ondřej Marek <ondrej.marek10@gmail.com>
2020-12-31 15:58:40 +0100

    vylepšený systém přidávání otázek

--- Models/QSet.cs
+++ Models/QSet.cs
@@ -14,29 +14,32 @@ namespace Millionaire.Models
         List<Question> mediumQuestions = new List<Question>();
         List<Question> hardQuestions = new List<Question>();
 
-
         public List<Question> EasyQuestions { get { return easyQuestions; } }
         public List<Question> MediumQuestions { get { return mediumQuestions; } }
         public List<Question> HardQuestions { get { return hardQuestions; } }
+        
 
-        //public QSet(string name)
-        //{
-        //    Name = name;
-        //}
-
-        public void AddEasyQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        public void AddQuestion(Difficulty difficulty, string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
         {
-            easyQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+            switch (difficulty)
+            {
+                case Difficulty.easy:
+                    easyQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+                    break;
+                case Difficulty.medium:
+                    mediumQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+                    break;
+                case Difficulty.hard:
+                    hardQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
+                    break;
+            }
         }
+    }
 
-        public void AddMediumQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
-        {
-            mediumQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
-        }
-
-        public void AddHardQ(string question, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
-        {
-            hardQuestions.Add(new Question(question, rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3));
-        }
+    public enum Difficulty
+    {
+        easy,
+        medium,
+        hard
     }
 }
