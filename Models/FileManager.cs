285c195182908a165e9a12323dd03398f6d29db3
Ondřej Marek <ondrej.marek10@gmail.com>
2020-12-31 15:59:46 +0100

    vylepšená metoda načítání ze souboru, nový systém nakládání s výjimkami

--- Models/FileManager.cs
+++ Models/FileManager.cs
@@ -14,18 +14,17 @@ namespace Millionaire.Models
         {
             get
             {
-                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Millionaire");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                return path;
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Millionaire");
             }
         }
         private string defaultQSetsPath = @"..\..\DefaultQSets";
 
         public FileManager()
-        {         
+        {
+            if (!Directory.Exists(dataDir))
+            {
+                Directory.CreateDirectory(dataDir);
+            }
         }
 
         /// <summary>
@@ -55,105 +54,164 @@ namespace Millionaire.Models
             }
         }
 
-        public List<QSet> LoadQuestionSets()
+        /// <summary>
+        /// Loads all question sets from a program's folder
+        /// </summary>
+        /// <param name="exceptions">Out param., returns list of potential exceptions</param>
+        /// <returns>List of question sets</returns>
+        public List<QSet> LoadQuestionSets(out List<Exception> exceptions)
         {
             List<QSet> qSets = new List<QSet>();
-            QSet qSet;
+            QSet qSet = null;
+
+            exceptions = new List<Exception>();
             
             string[] files = Directory.GetFiles(dataDir, "*.csv");
             foreach (string file in files)
             {
-                qSet = LoadQSetFromFile(file);
+                try
+                {
+                    qSet = LoadQSetFromFile(file);
+                }
+                catch(Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+                
                 if(qSet != null)
                 {
                     qSets.Add(qSet);
                 }                                
             }
-
-            Debug.WriteLine(qSets.Count());
-
+            
             return qSets;
         }
 
+
+        /// <summary>
+        /// Loads one question set from a file
+        /// </summary>
+        /// <param name="path">File location</param>
+        /// <returns>Question set or null if file's content is invalid</returns>
         public QSet LoadQSetFromFile(string path)
         {
             QSet qSet = new QSet();
-
             int counter = 0;
 
             using (StreamReader sr = new StreamReader(path))
             {
                 string s = sr.ReadLine();
-                if (s == null)
+                if (s == null) //checks wheter the first line is not null
                 {
                     return null;
                 }
-
                 qSet.Name = s.TrimEnd(';');
 
                 sr.ReadLine();
 
+                Difficulty difficulty = Difficulty.easy;
                 s = sr.ReadLine();
-                while (s != null && !s.StartsWith("*"))
+                while(s != null)
                 {
+                    if (s.StartsWith("*")) //checks wheter higher difficulty section was reached
+                    {
+                        if (counter < 5) //checks wheter enough questions of previous difficulty was loaded
+                        {
+                            return null;
+                        }
+
+                        if (difficulty < Difficulty.hard) //checks wheter highest difficulty level was reached
+                        {
+                            difficulty++;
+                            s = sr.ReadLine();
+                        }
+                        else
+                        {
+                            return null;
+                        }                        
+                    }
+
                     string[] split = s.Split(';');
-                    if (split.Length != 5)
+                    if (split.Length != 5) //checks wheter the line contains exactly 5 strings
                     {
                         return null;
                     }
-                    qSet.AddEasyQ(split[0], split[1], split[2], split[3], split[4]);
+                    qSet.AddQuestion(difficulty, split[0], split[1], split[2], split[3], split[4]);
                     counter++;
 
                     s = sr.ReadLine();
                 }
 
-                if (counter < 5)
-                {
-                    return null;
-                }
-                counter = 0;
 
-                s = sr.ReadLine();
-                while (s != null && !s.StartsWith("*"))
-                {
-                    string[] split = s.Split(';');
-                    if (split.Length != 5)
-                    {
-                        return null;
-                    }
-                    qSet.AddMediumQ(split[0], split[1], split[2], split[3], split[4]);
-                    counter++;
 
-                    s = sr.ReadLine();
-                }
 
-                if (counter < 5)
-                {
-                    return null;
-                }
-                counter = 0;
 
-                s = sr.ReadLine();
-                while (s != null && !s.StartsWith("*"))
-                {
-                    string[] split = s.Split(';');
-                    if (split.Length != 5)
-                    {
-                        return null;
-                    }
-                    qSet.AddHardQ(split[0], split[1], split[2], split[3], split[4]);
-                    counter++;
 
-                    s = sr.ReadLine();
-                }
+                //while (s != null && !s.StartsWith("*"))
+                //{
+                //    string[] split = s.Split(';');
+                //    if (split.Length != 5)
+                //    {
+                //        return null;
+                //    }
+                //    qSet.AddEasyQ(split[0], split[1], split[2], split[3], split[4]);
+                //    counter++;
 
-                if (counter < 5)
-                {
-                    return null;
-                }               
+                //    s = sr.ReadLine();
+                //}
+
+                //if (counter < 5)
+                //{
+                //    return null;
+                //}
+                //counter = 0;
+
+                //s = sr.ReadLine();
+                //while (s != null && !s.StartsWith("*"))
+                //{
+                //    string[] split = s.Split(';');
+                //    if (split.Length != 5)
+                //    {
+                //        return null;
+                //    }
+                //    qSet.AddMediumQ(split[0], split[1], split[2], split[3], split[4]);
+                //    counter++;
+
+                //    s = sr.ReadLine();
+                //}
+
+                //if (counter < 5)
+                //{
+                //    return null;
+                //}
+                //counter = 0;
+
+                //s = sr.ReadLine();
+                //while (s != null && !s.StartsWith("*"))
+                //{
+                //    string[] split = s.Split(';');
+                //    if (split.Length != 5)
+                //    {
+                //        return null;
+                //    }
+                //    qSet.AddHardQ(split[0], split[1], split[2], split[3], split[4]);
+                //    counter++;
+
+                //    s = sr.ReadLine();
+                //}
+
+                //if (counter < 5)
+                //{
+                //    return null;
+                //}
 
             }
-            Debug.WriteLine("vrátil jsem hodnoty");
+
+            foreach(Question question in qSet.MediumQuestions)
+            {
+                Debug.WriteLine(question.question);
+            }
+            
             return qSet;
         }
 
